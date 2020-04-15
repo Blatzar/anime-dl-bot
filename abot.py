7413157ccf970679e95d3b5822403e5c05e1055c
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-04-15 20:37:24 +0000

    changed to animeflix and fixed stuff

--- abot.py
+++ abot.py
@@ -1,30 +1,37 @@
 import discord
 from discord.ext import commands
-import os
 import time
 import json
 from anime_downloader.sites import get_anime_class
-import testkeys #my personal token
-Provider = get_anime_class('animeout')
-testkey = testkeys.token() #Secret token
-token = testkey.anime
+
+try:
+	import testkeys 
+	testkey = testkeys.token() 
+	token = testkey.anime 
+except:
+	token = ''
+
+Provider = get_anime_class('animeflix')
 bot = commands.Bot(command_prefix='')
+path = '/home/ec2-user/keep/anime.json' #stores all user search
 
-def Int(number):
-    try:
-        number = int(number)
-        return(True)
-    except:
-        return(False)
+def Int(number): #Can it be converted to int
+	try:
+		number = int(number)
+		return(True)
+	except:
+		return(False)
 
-def Write(f, path='$HOME/keep/anime.json'): #I couldn't be bothered to learn proper file opening, please fix this in a PR
-    f = str(f).replace("'",'"')
-    print(f)
-    os.system(f"echo '{f}' > {path}")
+def Write(f, path = path):
+	f = str(f).replace("'",'"')
+	#print(f)
+	_file = open(path, "w")
+	_file.write(f)
+	_file.close()
 
-def Load(path = '$HOME/keep/anime.json'): #All user info in anime.json
-    f = json.loads(os.popen(f'cat {path}').read())
-    return(f)
+def Load(path = path): #All user info in anime.json
+	f = json.loads(open(path).read())
+	return(f)
 
 #send a pm with a discord ID
 async def sendPM(ID,message):
@@ -36,22 +43,28 @@ async def GetUserName(ID):
 		name = bot.get_user(ID)
 		return(str(name.name))
 
-def Message(message,prefix):
+def Message(message,prefix): #0 is True/False, 1 is the string after
 	return [message.content[:len(prefix)] == prefix and len(message.content) > len(prefix),message.content[len(prefix):]]
 
 @bot.event
 async def on_message(message):
-
-	#search
+	
 	search = Message(message,'a ')
 	select = Message(message,'s ')
 	episode = Message(message,'e ')
+	
+	#search
 	if search[0]:
 		print(search[1])
 		results,results_list = Search(search[1])
 		f = Load()
 		[f.remove(a) for a in f if a['user'] == message.author.id]
-		f.append({"user":message.author.id,"episode":0,"data":[{"url":a.url,"title":a.title} for a in results_list]})
+		f.append({
+			"user":message.author.id,
+			"select":{"url":"","title":""},
+			"episode":0,
+			"data":[{"url":a.url,"title":a.title} for a in results_list]
+			})
 		Write(f)
 		await message.channel.send(results)
 	
@@ -61,41 +74,58 @@ async def on_message(message):
 		for a in f:
 			if a["user"] == message.author.id and Int(select[1]):
 				select[1] = int(select[1]) - 1 #Starts from 1
-				await message.channel.send('Selected: ' + a["data"][select[1]]["title"] + f' **Episode: {a["episode"]+1}**')
-				anime = Provider(a["data"][select[1]]["url"])
-				#print(anime)
-				embed = discord.Embed(
-					title = a["data"][select[1]]["url"],
-					url = anime[0].source().stream_url
-				)
-				try:await message.channel.send(embed=embed)
-				except:await message.channel.send(anime[0].source().stream_url)
+				await message.channel.send('Selected: ' + a["data"][select[1]]["title"] + f' - **Episode: {a["episode"]+1}**')
+				a["select"]["url"],a["select"]["title"] = a["data"][select[1]]["url"],a["data"][select[1]]["title"]
 				Write(f)
-
+				try:
+					anime = Provider(a["data"][select[1]]["url"])
+					embed = discord.Embed(
+						title = a["data"][select[1]]["title"]+f' - Episode {a["episode"]+1}/{len(anime)}',
+						url = anime[a["episode"]].source().stream_url
+					)
+				except:
+					await message.channel.send('Error selecting episode')
+				try:await message.channel.send(embed=embed)
+				except:await message.channel.send(anime[a["episode"]].source().stream_url)
+	#list
 	if message.content == 'l':
 		f = Load()
 		for a in f:
 			if a["user"] == message.author.id:
-				results = ''
+				results = '```'
 				for b in range(len(a["data"])):
-					results = f'{results}\n**{str(b+1)}** {str(a["data"][b]["title"])}'
+					results = f'{results}\n{str(a["data"][b]["title"])}'
+				results += '```'
 				await message.channel.send(results)
-			
+	#change episode	
 	if episode[0]:
 		f = Load()
 		if Int(episode[1]):
 			for a in f:
 				if a['user'] == message.author.id: 
-					a["episode"] = int(episode[1])+1
+					a["episode"] = int(episode[1])-1
 					Write(f)
+					await message.channel.send(f'Selected episode {episode[1]}')
+					try:
+						anime = Provider(a["select"]["url"])
+						embed = discord.Embed(
+						title = a["select"]["title"]+f' - Episode {a["episode"]+1}/{len(anime)}',
+						url = anime[a["episode"]].source().stream_url
+						)
+					except:
+						await message.channel.send('Error selecting episode')
+					try:await message.channel.send(embed=embed)
+					except:await message.channel.send(anime[a["episode"]].source().stream_url)
+
 	if message.content == 'help':
-		await message.channel.send('a *anime* to search\ns *number* to select anime\ne *number* to select episode')
+		await message.channel.send('a *anime* to search\ns *number* to select anime\ne *number* to select episode\nl to list search')
 def Search(query):
-	results = ''
+	results = '```'
 	print(f'Query: {query}')
 	search = Provider.search(query)
 	for a in range(len(search)):
-		results = f'{results}\n**{str(a+1)}** {str(search[a])}'
+		results = f'{results}\n{str(search[a])}'
+	results += '```'
 	print(results)
 	return(results,search)
 
